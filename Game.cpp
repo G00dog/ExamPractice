422318ab10eb022b2f913432458762b075415e8b
20181185신준원 <83389447+G00dog@users.noreply.github.com>
2021-10-28 11:54:19 +0000

    원본상자 대상상자 응용4

--- Game.cpp
+++ Game.cpp
@@ -36,17 +36,17 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
   SDL_FreeSurface(pTempSurface);
 
   SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
-  m_sourceRectangle.x = 50;
-  m_sourceRectangle.y = 50;
+  m_sourceRectangle.x = 0;
+  m_sourceRectangle.y = 0;
 
-  m_sourceRectangle.w = 50;
-  m_sourceRectangle.h = 50;
+  //m_sourceRectangle.w = 50;
+  //m_sourceRectangle.h = 50;
 
-  m_destinationRectangle.w = m_sourceRectangle.w;
-  m_destinationRectangle.h = m_sourceRectangle.h;
+  m_destinationRectangle.w = 640;
+  m_destinationRectangle.h = 480;
 
-  m_destinationRectangle.x = 50;
-  m_destinationRectangle.y = 50;
+  m_destinationRectangle.x = 0;
+  m_destinationRectangle.y = 0;
 
   m_bRunning = true;
   return true;
